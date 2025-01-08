63da6c7ad03f45e2b2481d48a1b7c3b2c965ef48
lsngmin <lsngmin@naver.com>
2025-01-09 00:27:25 +0900

    Jwt Security Setting

--- src/main/java/com/example/demo/config/CustomSecurityConfig.java
+++ src/main/java/com/example/demo/config/CustomSecurityConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -16,6 +18,13 @@ import org.springframework.security.web.SecurityFilterChain;
 public class CustomSecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.formLogin(AbstractHttpConfigurer::disable);
+        http.logout(config-> config.disable());
+        http.csrf(config-> config.disable());
+        http.sessionManagement(sessionManagementConfigurer -> {
+            sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.NEVER);
+        });
+
         return http.authorizeHttpRequests((auth) -> auth
                         .requestMatchers("/", "/login", "api/v1/signup", "api/v1/user/**", "/error", "/*").permitAll()
                         .anyRequest().authenticated()
